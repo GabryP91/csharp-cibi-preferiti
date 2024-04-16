b1107cb4f5b6e73c5b7fb08385bdbae41e9aeb15
Gabriele <gabriele.palma.bool@gmail.com>
2024-04-16 18:58:50 +0200

    add

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -14,20 +14,20 @@ namespace csharp_cibi_preferiti
             string[] MioArray = { "pizza", "mandarino", "cicoria", "tonno", "bistecca di manzo", "peperone", "carota", "mela" };
 
             //Stampo l'unghezza effettiva array
-            Console.WriteLine($"\nLa Lunghezza dell'array è: {MioArray.Length}\n");
+            Console.WriteLine($"\n La Lunghezza dell'array è: {MioArray.Length}\n");
 
             //Scorro tutto l'array ed estraggo le informazioni per ogni posizione
             for (int i = 0; i < MioArray.Length; i++)
             {
                 //per ogni posizione dell'array stampo il valore corrispondente
-                Console.WriteLine($"Il valore in posizione {i} è: {MioArray[i]}\n");
+                Console.WriteLine($" Il valore in posizione {i} è: {MioArray[i]}\n");
             }
 
             //Stampo il valore in posizione 0
-            Console.WriteLine($"\nIl valore in posizione 0 è: {MioArray[0]}\n");
+            Console.WriteLine($"\n Il valore in posizione 0 è: {MioArray[0]}\n");
 
             //Stampo il valore in ultima posizoione
-            Console.WriteLine($"\nIl valore in ultima posizione è: {MioArray[MioArray.Length - 1]}\n");
+            Console.WriteLine($"\n Il valore in ultima posizione è: {MioArray[MioArray.Length - 1]}\n");
 
             //ottengo indice medio del mio array
             int val = MioArray.Length / 2;
@@ -36,7 +36,7 @@ namespace csharp_cibi_preferiti
             if (val % 2 == 0)
             {
                 //Stampo il valore corrispondente e il precedente
-                Console.WriteLine($"\nGli elementi centrale saranno: {MioArray[val - 1]} e {MioArray[val]} \n");
+                Console.WriteLine($"\n Gli elementi centrale saranno: {MioArray[val - 1]} e {MioArray[val]} \n");
                 
             }
 
@@ -44,7 +44,7 @@ namespace csharp_cibi_preferiti
             else
             {
                 //Stampo il valore corrispondente
-                Console.WriteLine($"\nL'elemento centrale sarà: {MioArray[val]}\n");
+                Console.WriteLine($"\n L'elemento centrale sarà: {MioArray[val]}\n");
             }
 
             Console.ReadKey();
